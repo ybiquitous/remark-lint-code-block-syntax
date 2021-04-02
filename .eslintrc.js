fe8c39a68e7f14e19239b1b16df405ca838fda32
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-04-03 01:50:53 +0900

    Set up lint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module",
+  },
+  env: {
+    es2017: true,
+    node: true,
+    jest: true,
+  },
+};
